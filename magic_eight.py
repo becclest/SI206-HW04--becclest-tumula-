e8474af2a4bff3ebe68bd24bf1c944ccee5ba8c9
Brianna Eccleston <becclest@umich.edu>
2018-02-02 20:41:58 -0500

    person A pt. 2

--- magic_eight.py
+++ magic_eight.py
@@ -3,3 +3,8 @@ def greetingQuestion():
     print("Nice to meet you " + name + "!")
     questionR = input("What do you want to learn?")
     print("Well, " + name + "we will try to figure out the answer together!")
+
+
+def checkQuestion(arg):
+    if input [-1] != "?":
+        print(“I’m sorry, I can only answer questions.”)
