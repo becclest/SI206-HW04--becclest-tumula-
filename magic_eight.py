a90f04ffb5a53b8f5567ecea5715349c653730b7
Brianna Eccleston <becclest@umich.edu>
2018-02-02 20:39:32 -0500

    initial commit

---
+++ magic_eight.py
@@ -0,0 +1,5 @@
+def greetingQuestion():
+    name = input("What's your name?")
+    print("Nice to meet you " + name + "!")
+    questionR = input("What do you want to learn?")
+    print("Well, " + name + "we will try to figure out the answer together!")
