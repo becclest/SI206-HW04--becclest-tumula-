ff70479c539f54c54f0343b07d1d8ccfe20ee74e
Brianna Eccleston <becclest@umich.edu>
2018-02-04 11:50:21 -0500

    Fixed duplicate functions

--- magic_eight.py
+++ magic_eight.py
@@ -1,42 +1,33 @@
 import random
 
-def questionAnswer():
+def greeting():
     name = input("What's your name?")
     print("Nice to meet you " + name + "!")
-<<<<<<< HEAD
-    questionR = input("What is your question?")
-    if questionR [-1] != "?":
-        print('I\'m sorry, I can only answer questions.')
-    else:
-        possibleAnswers = ['It is certain',
-                       'It is decidedly so',
-                       'Without a doubt',
-                       'Yes definitely',
-                       'You may rely on it',
-                       'As I see it, yes',
-                       'Most likely',
-                       'Outlook good',
-                       'Yes',
-                       'Signs point to yes',
-                       'Reply hazy try again',
-                       'Ask again later',
-                       'Better not tell you now',
-                       'Cannot predict now',
-                       'Concentrate and ask again',
-                       'Don\'t count on it',
-                       'My reply is no',
-                       'My sources say no',
-                       'Outlook not so good',
-                       'Very doubtful']
-        print(random.choice(possibleAnswers))
 
-questionAnswer()
-=======
-    questionR = input("What do you want to learn?")
-    print("Well, " + name + "we will try to figure out the answer together!")
+greeting()
 
-
-def checkQuestion(arg):
-    if input [-1] != "?":
-        print(“I’m sorry, I can only answer questions.”)
->>>>>>> check_question
+questionR = input("What is your question?")
+if questionR [-1] != "?":
+    print('I am sorry, I can only answer questions.')
+else:
+    possibleAnswers = ['It is certain',
+       'It is decidedly so',
+       'Without a doubt',
+       'Yes definitely',
+       'You may rely on it',
+       'As I see it, yes',
+       'Most likely',
+       'Outlook good',
+       'Yes',
+       'Signs point to yes',
+       'Reply hazy try again',
+       'Ask again later',
+       'Better not tell you now',
+       'Cannot predict now',
+       'Concentrate and ask again',
+       'Don\'t count on it',
+       'My reply is no',
+       'My sources say no',
+       'Outlook not so good',
+       'Very doubtful']
+    print(random.choice(possibleAnswers))
